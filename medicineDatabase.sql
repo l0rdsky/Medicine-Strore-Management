c44aefc1606209c24d9b4ad27c1294efafb4b4a9
Akash pawar <115169844+l0rdsky@users.noreply.github.com>
2023-05-28 23:32:26 +0530

    Update medicineDatabase.sql

--- medicineDatabase.sql
+++ medicineDatabase.sql
@@ -4,3 +4,7 @@ create table DBUsers( Username varchar(20), Password varchar(50));
 insert into DBUsers( Username, Password) values("akash","34");
 select * from DBUsers;
 insert into DBUsers( Username, Password) values("jainam","41");
+
+create table store( item varchar(20), quantity int , price float); 
+insert into store values("iodex",10,34.34);
+select * from store;
